f88d298f290038f7f661f31017ea9f32af0091df
Yaeyoung Rigby <yyrigby@gmail.com>
2016-05-30 22:03:48 -0700

    updated the routes file

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'main';
+$route['default_controller'] = "main";
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['games/update_game'] = "games/update_game";
+$route['profile'] = "main/profile";
+
+//User related controller
+$route['logoff'] = 'users/logoff';
+$route['users/signin'] = 'users/login';
+$route['users/register'] = 'users/register';
+
+
+//Load chess pages
+$route['view_game/(:any)'] = "games/view_game/$1";
+$route['my_games'] = 'games/my_games';
+$route['paste_pgn'] = 'main/paste_pgn';
+
+
+//chess related controller
+
+
+//Edit Personal Profile controllers
+$route['users/edit'] = 'users/edit_info';
+$route['users/update_password'] = 'users/update_password';
